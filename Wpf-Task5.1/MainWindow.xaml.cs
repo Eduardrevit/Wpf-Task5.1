3341c653c55a7fcf563c27e56b6ede141e87703a
esg-eduard@mail.ru <esg-eduard@mail.ru>
2023-01-07 14:33:56 +0300

    Добавлен словарь ресурсов

--- Wpf-Task5.1/MainWindow.xaml.cs
+++ Wpf-Task5.1/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace Wpf_Task5._1
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = ((sender as ComboBox).SelectedItem as string);
             if (textBox != null)
             {
                 textBox.FontFamily = new FontFamily(fontName);
@@ -45,7 +45,7 @@ namespace Wpf_Task5._1
         {
             if (textBox != null)
             {
-                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem));
                 textBox.FontSize = fontSize;
             }
         }
